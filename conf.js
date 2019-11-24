2e83d8595f7a60cb3a822215d4747fbb432c1b50
student <student@localhost.localdomain>
2019-11-24 03:09:49 -0500

    completed isbn 10 digit check

--- conf.js
+++ conf.js
@@ -1,3 +1,3 @@
 module.exports = {
-  showProcess:1
+  showProcess:0
 }
--- isbn.js
+++ isbn.js
@@ -12,18 +12,17 @@ module.exports = class isbn{
       output: process.stdout
     });
 
-    readLine.question('ISBN 13 : ', function(inISBN){
-      this.inputedISBN = inISBN;
+    readLine.question('ISBN : ', function(inISBN){
+      this.inputedISBN = inISBN.replace(/[^0-9]/g,'');
       console.log(`You entered ISBN ${this.inputedISBN.length}`);
       switch(this.inputedISBN.length){
         case 13:
           this.isbn13;
           break;
         case 10:
-          console.log('isbn 10 calculation is not available yet');
+          this.isbn10;
           break;
       }
-
       readLine.close();
     }.bind(this));
 
@@ -31,6 +30,10 @@ module.exports = class isbn{
 
   get isbn13(){
     this.__isbn13();
+  }
+
+  get isbn10(){
+    this.__isbn10();
   }
 
   __isbn13(){
@@ -101,6 +104,45 @@ module.exports = class isbn{
 
   }
 
+  __isbn10(){
+    let isbn10 = "0" + this.inputedISBN;
+    let arr = isbn10.split("");
+    let the_result = [];
+    if(!!conf.showProcess){
+      console.log("\x1b[4m", "Step 1: First 9 digits x [10...2] = the result", "\x1b[0m");
+    }
+    for(let i=10; i>1; i--){
+      the_result.push( arr[11-i] * i );
+      if(!!conf.showProcess){
+        console.log(arr[11-i], 'x'  , i, ' = ', arr[11-i] * i );
+        console.log("\x1b[4m", "Step 2: Add the results", "\x1b[0m");
+      }
+    }
+    let total = the_result.reduce((a,b)=>{return a+b});
+    if(!!conf.showProcess){
+      console.log(the_result.join(' + ') , " = ", total);
+      console.log("\x1b[4m", "Step 3: modulo 11", "\x1b[0m");
+    }
+    let remainder = total % 11;
+    if(!!conf.showProcess){
+      console.log(`${total} mod 11 = ${remainder}`);
+      console.log("\x1b[4m", "Step 4: check digit", "\x1b[0m");
+    }
+    let check_digit = ((remainder === 0) ? 0 : 11 - remainder);
+    if(remainder !== 0 && !!conf.showProcess){
+      console.log(`11 - ${remainder} = ${check_digit}`);
+      console.log(`check digit : ${check_digit}`);
+    }
+
+    let inputed_check_digit = parseInt(arr[10]);
+    let calculated_check_digit =  check_digit;
+
+    if(inputed_check_digit === calculated_check_digit){
+      console.log('ISBN number is correct');
+    }else{
+      console.log('ISBN number is incorrect');
+    }
+  }
 }
 
 
